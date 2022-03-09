f9fefe983ce5759429811820948cec9c9aec381b
sanyue1911 <sanyue1911@163.com>
2022-03-09 11:20:39 +0800

    定时器

---
+++ timer/list_timer.h
@@ -0,0 +1,106 @@
+#ifndef LST_TIMER
+#define LST_TIMER
+
+#include <unistd.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/epoll.h>
+#include <fcntl.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <assert.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/mman.h>
+#include <stdarg.h>
+#include <errno.h>
+#include <sys/wait.h>
+#include <sys/uio.h>
+
+#include <time.h>
+#include "../log/log.h"
+
+class util_timer;
+
+struct client_data
+{
+    sockaddr_in address;
+    int sockfd;
+
+    //定时器
+    util_timer *timer;
+};
+
+class util_timer
+{
+public:
+    util_timer() : prev(NULL), next(NULL) {}
+
+public:
+    time_t expire; //超时时间
+    
+    void (* cb_func)(client_data *);    //回调函数
+    client_data *user_data;             //连接资源
+    util_timer *prev;                   //前驱
+    util_timer *next;                   //后继
+};
+
+//定时器容器类
+class sort_timer_lst
+{
+public:
+    sort_timer_lst();
+    ~sort_timer_lst();
+
+    void add_timer(util_timer *timer);      //添加定时器
+    void adjust_timer(util_timer *timer);   //调整定时器
+    void del_timer(util_timer *timer);      //删除定时器
+
+    void tick();    //定时处理到期定时器函数
+
+private:
+    void add_timer(util_timer *timer, util_timer *lst_head);
+
+    util_timer *head;   //头节点
+    util_timer *tail;   //尾节点
+};
+
+class Utils
+{
+public:
+    Utils() {}
+    ~Utils() {}
+
+    void init(int timeslot);
+
+    //对文件描述符设置非阻塞
+    int setnonblocking(int fd);
+
+    //将内核事件表注册读事件，ET模式，选择开启EPOLLONESHOT
+    void addfd(int epollfd, int fd, bool one_shot, int TRIGMode);
+
+    //信号处理函数
+    static void sig_handler(int sig);
+
+    //设置信号函数
+    void addsig(int sig, void(handler)(int), bool restart = true);
+
+    //定时处理任务，重新定时以不断触发SIGALRM信号
+    void timer_handler();
+
+    void show_error(int connfd, const char *info);
+
+public:
+    static int *u_pipefd;   //管道
+    sort_timer_lst m_timer_lst; //定时器容器
+    static int u_epollfd;       //epoll
+    int m_TIMESLOT;         //触发处理任务时间间隔
+};
+
+void cb_func(client_data *user_data);
+
+#endif
